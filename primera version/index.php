c7900caa08184ef8b066b53e890fbbdb26ad1eb5
DanielColorado <daniel.colorado.tostado@hotmail.com>
2015-02-11 20:40:46 -0600

    primera version

--- primera version/index.php
+++ primera version/index.php
@@ -2,9 +2,11 @@
 require_once ('lib/initialize.php');
 $alumno=new Alumno('Rocio', 'Bravo', 'Rojas', '21', 'rocio@hotmail.com', '2-13-11-25', NULL, 'Amozoc de mota', '1','2');
 $alumno->guardar();
+//$alumno = Alumno::buscar_por_id('19');
+//$alumno->eliminar();
 $registros = Alumno::buscar_todos();
 foreach ($registros as $persona) {
-    echo $persona->toString().'<br/><br/>';
+    echo $persona->getId().'<br/>'.$persona->toString().'<br/><br/>';
 }
 ?>
 
